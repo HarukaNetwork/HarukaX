77121e116034b6f6b32e4ab78c01446143f09704
ATechnoHazard <amolele@gmail.com>
2019-10-24 22:47:51 +0530

    Rollback to default upsert behaviour
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- go_bot/modules/sql/users_sql.go
+++ go_bot/modules/sql/users_sql.go
@@ -36,7 +36,7 @@ func UpdateUser(userId int, username string, chatId string, chatName string) {
 
 	// upsert user
 	user := &User{UserId: userId, UserName: username}
-	tx.Save(user)
+	tx.Where(User{UserId:userId}).Assign(User{UserName:username}).FirstOrCreate(user)
 
 	if chatId == "nil" || chatName == "nil" {
 		return
@@ -44,7 +44,7 @@ func UpdateUser(userId int, username string, chatId string, chatName string) {
 
 	// upsert chat
 	chat := &Chat{ChatId: chatId, ChatName: chatName}
-	tx.Save(chat)
+	tx.Where(Chat{ChatId:chatId}).Assign(Chat{ChatName:chatName}).FirstOrCreate(chat)
 	tx.Commit()
 }
 
