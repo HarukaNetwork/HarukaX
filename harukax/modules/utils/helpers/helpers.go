9a14344d4b174636bff663156abbe12ad146162c
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2020-08-09 22:37:49 +0530

    Added Contains and Fixed Echo

--- harukax/modules/utils/helpers/helpers.go
+++ harukax/modules/utils/helpers/helpers.go
@@ -222,6 +222,15 @@ func GetNoteType(msg *ext.Message) (string, string, int, string, []tg_md2html.Bu
 	return noteName, text, dataType, content, buttons
 }
 
+func Contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
+
 func GetWelcomeType(msg *ext.Message) (string, int, string, []tg_md2html.Button) {
 	text := ""
 	var dataType = -1
